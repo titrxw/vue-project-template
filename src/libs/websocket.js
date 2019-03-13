c4ca8ece4d3e9b151b7a3212846912aba9a7f617
titrxw <1120309488@qq.com>
2019-03-13 21:55:19 +0800

    change

--- src/libs/websocket.js
+++ src/libs/websocket.js
@@ -1,47 +1,56 @@
 class MyWebSocket {
-  _onError = null;
-  _onConnect = null;
-  _onMessage = null;
-  _handle = null;
+    _onError = null;
+    _onConnect = null;
+    _onClose = null;
+    _onMessage = {};
+    _handle = null;
 
-  setOnConnect(callback) {
-    this._onConnect = callback;
-  }
-  setOnError(callback) {
-    this._onError = callback;
-  }
-  setOnMessage(callback) {
-    this._onMessage = callback;
-  }
-
-  connect(host) {
-    if (this._handle) {
-      return true;
+    setOnConnect(callback) {
+        this._onConnect = callback;
     }
-    let that = this
-    this.handle = new WebSocket(host);
-    this.handle.onerror = function (event) {
-      that._onError && that._onError(event);
-    };
-    //与WebSocket建立连接
-    this.handle.onopen = function (event) {
-      that._onConnect && that._onConnect(event);
-    };
-    //处理服务器返回的信息
-    this.handle.onmessage = function (event) {
-      that._onMessage && that._onMessage(event);
-    };
-  }
-  send(data) {
-    if (typeof data == 'object') {
-      data = JSON.stringify(data)
+    setOnError(callback) {
+        this._onError = callback;
     }
-    this.handle.send(data)
-  }
-}
+    setOnClose(callback) {
+        this._onClose = callback;
+    }
+    setOnMessage(callback, index = 1) {
+        this._onMessage[index] = callback;
+    }
 
-export default {
-  install: function (Vue) {
-    Vue.prototype.$websocket = new MyWebSocket()
-  }
+    connect(host) {
+        if (this._handle) {
+            return true;
+        }
+
+        this._handle = new WebSocket(host);
+        this._handle.onerror = (event) => {
+            this._handle = null
+            this._onError && this._onError(event);
+        };
+        //与WebSocket建立连接
+        this._handle.onopen = (event) => {
+            this._onConnect && this._onConnect(event);
+        };
+        //处理服务器返回的信息
+        this._handle.onmessage = (event) => {
+            let callback = Object.values(this._onMessage)
+            callback.reduce(function(data, curCallback) {
+                return curCallback(data)
+            }, event.data)
+        };
+        this._handle.onclose = (event) => {
+            this._handle = null
+            this._onClose && this._onClose(event);
+        };
+    }
+    send(data) {
+        if (typeof data == 'object') {
+            data = JSON.stringify(data)
+        }
+        this._handle.send(data)
+    }
+    close() {
+        this._handle.close()
+    }
 }
