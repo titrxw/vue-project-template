721ee64b2a170fa38b52a6663f436ea7bd354059
titrxw <1120309488@qq.com>
2019-03-18 21:51:38 +0800

    change

--- src/libs/websocket.js
+++ src/libs/websocket.js
@@ -1,56 +1,132 @@
-class MyWebSocket {
+export default class MyWebSocket {
+    host = null;
+
     _onError = null;
     _onConnect = null;
     _onClose = null;
-    _onMessage = {};
+    _onMessage = null;
     _handle = null;
+    _tryNum = 3;
+    _hasTryNum = 0;
+    _isHeartBeat = true;
+    _pongHandle = null
+    _pingHandle = null;
 
-    setOnConnect(callback) {
+
+
+    constructor(tryNum = 3, heartBeat = true) {
+        this._tryNum = tryNum
+        this._isHeartBeat = heartBeat
+    }
+
+    startHeartbeat() {
+        if (!this._isHeartBeat) {
+            return false
+        }
+        this._pingHandle = setInterval(() => {
+            this.send('ping')
+            this._pongHandle = setTimeout(() => {
+                wx.closeSocket()
+            }, 5000)
+        }, 15000)
+    }
+
+    stopHeartbeat() {
+        if (!this._isHeartBeat) {
+            return false
+        }
+        clearInterval(this._pingHandle)
+        this._pingHandle = null
+        this.checkPong()
+    }
+
+    checkPong() {
+        if (this._pongHandle) {
+            clearTimeout(this._pongHandle);
+            this._pongHandle = null;
+        }
+    }
+
+    onConnect(callback) {
         this._onConnect = callback;
     }
-    setOnError(callback) {
-        this._onError = callback;
-    }
-    setOnClose(callback) {
+
+    onClose(callback) {
         this._onClose = callback;
     }
-    setOnMessage(callback, index = 1) {
-        this._onMessage[index] = callback;
+
+    onError(callback) {
+        this._onError = callback;
     }
 
-    connect(host) {
+    onMessage(callback) {
+        this._onMessage = callback;
+    }
+
+    connect(url) {
         if (this._handle) {
             return true;
         }
 
-        this._handle = new WebSocket(host);
+        this.host = url
+
+        this._handle = new WebSocket(url);
         this._handle.onerror = (event) => {
             this._handle = null
             this._onError && this._onError(event);
         };
         //与WebSocket建立连接
         this._handle.onopen = (event) => {
+            this._handle = true
+            this._hasTryNum = 0
+            this.startHeartbeat()
             this._onConnect && this._onConnect(event);
         };
         //处理服务器返回的信息
         this._handle.onmessage = (event) => {
-            let callback = Object.values(this._onMessage)
-            callback.reduce(function(data, curCallback) {
-                return curCallback(data)
-            }, event.data)
+            this.checkPong()
+            this._onMessage && this._onMessage(event.data);
         };
         this._handle.onclose = (event) => {
-            this._handle = null
-            this._onClose && this._onClose(event);
+            this._handle = false
+            this.stopHeartbeat()
+            if (event.reason != 'client-close') {
+                if (this._tryNum <= 0) {
+                    this._onClose && this._onClose(event);
+                } else {
+                    this.reConnect()
+                }
+            } else {
+                this._hasTryNum = 0
+            }
         };
     }
+
     send(data) {
+        if (!this._handle) {
+            return false
+        }
         if (typeof data == 'object') {
             data = JSON.stringify(data)
         }
         this._handle.send(data)
     }
+
     close() {
-        this._handle.close()
+        this._handle.close({
+            reason: 'client-close'
+        })
+    }
+
+    reConnect() {
+        if (this._hasTryNum == this._tryNum) {
+            this._onClose && this._onClose({
+                reason: 'abnormal'
+            });
+            return false
+        }
+
+        ++this._hasTryNum
+        this.connect(this.host)
     }
 }
